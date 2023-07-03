c6a5a4c7e5ecb5a6266be4efe5945169eeebd7aa
kirwana eric roberts <130729691+kirwanaEricRoberts256@users.noreply.github.com>
2023-07-03 16:10:06 +0300

    updated the menu

--- src/pages/navbar.js
+++ src/pages/navbar.js
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 import './navbar.css';
 import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 export default function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -13,15 +15,13 @@ export default function Navbar() {
     <div className="Navbar">
       <div className="Logo"><h1>@roberts</h1></div>
       <button className={`Hamburger ${menuOpen ? 'open' : ''}`} onClick={handleMenuToggle}>
-        <span className="Hamburger-line"></span>
-        <span className="Hamburger-line"></span>
-        <span className="Hamburger-line"></span>
+        <FontAwesomeIcon icon={faBars} />
       </button>
       <ul className={`Navbar-menu ${menuOpen ? 'open' : ''}`}>
         <li><Link to="/" className="Link">HOME</Link></li>
         <li><Link to="/about" className="Link">ABOUT ME</Link></li>
-        <li><Link to="/contact" className="Link">Contact</Link></li>
-        <li><Link to="/project" className="Link">My projects</Link></li>
+        <li><Link to="/contact" className="Link">CONTACT</Link></li>
+        <li><Link to="/project" className="Link">MY PROJECTS</Link></li>
       </ul>
     </div>
   );
