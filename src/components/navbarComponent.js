3abedf68f25d989d7b30ae29b539b19cebe5bd86
kirwana eric roberts <130729691+kirwanaEricRoberts256@users.noreply.github.com>
2023-07-04 14:27:41 +0300

    made an improvement on the menubar

--- src/components/navbarComponent.js
+++ src/components/navbarComponent.js
@@ -1,25 +1,42 @@
-
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+const CustomNavbar = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
-export default function CustomNavbar() {
-    return (
-      <Navbar collapseOnSelect expand="md" bg="gray" variant="gray">
-        <Container>
-          <Navbar.Brand href="#home">Logo</Navbar.Brand>
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const isMobile = windowWidth <= 576; // Adjust the breakpoint to match your needs
+
+  return (
+    <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">Logo</Navbar.Brand>
+        {isMobile ? (
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="mr-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#about">About</Nav.Link>
-              <Nav.Link href="#contact">Services</Nav.Link>
-              <Nav.Link href="#project">Project</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    );
-  };
-  
+        ) : null}
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#services">Services</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default CustomNavbar;
