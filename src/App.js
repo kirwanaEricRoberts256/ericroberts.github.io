c037fa4918b727eddf80eb5d5d75de11babf7d7d
kirwana eric roberts <130729691+kirwanaEricRoberts256@users.noreply.github.com>
2023-07-06 09:35:59 +0300

    imported a bootstrap code for the navbar

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <CustomNavbar/>
-      <Navbars/>
+      
       <Routes>
 <Route path="/" element={<Home/>}/>
 <Route path="/contact" element={<Contact/>}/>
--- src/components/navbarComponent.js
+++ src/components/navbarComponent.js
@@ -1,22 +1,65 @@
-import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Link} from "react-router-dom"
 
-const CustomNavbar = () => {
+function CustomNavbar() {
   return (
-    <Navbar bg="red" variant="red" expand="lg">
-      <Navbar.Brand href="#">Hidden brand</Navbar.Brand>
-      <Navbar.Toggle aria-controls="navbarCollapse" />
-      <Navbar.Collapse id="navbarCollapse">
-        <Nav className="ml-auto">
-          <Nav.Link href="#">Home</Nav.Link>
-          <Nav.Link href="about.js">About</Nav.Link>
-          <Nav.Link href="contact.js">Contacts</Nav.Link>
-          <Nav.Link href="project.js">Project</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+    <>
+      <Navbar expand="sm" className="bg-body-primary mb-3" bg="dark" variant="dark">
+        <Container fluid>
+          <Navbar.Brand href="#">Eric Roberts</Navbar.Brand>
+          <Navbar.Toggle aria-controls="offcanvasNavbar-expand-sm" />
+          <Navbar.Offcanvas
+            id="offcanvasNavbar-expand-sm"
+            aria-labelledby="offcanvasNavbarLabel-expand-sm"
+            placement="end"
+          >
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title id="offcanvasNavbarLabel-expand-sm">
+                Offcanvas
+              </Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className="justify-content-end flex-grow-1 pe-3">
+                <Nav.Link as={Link} to="/">Home</Nav.Link>
+                <Nav.Link as={Link} to="/about">About</Nav.Link>
+                <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+                <Nav.Link as={Link} to="/project">Projects</Nav.Link>
+                <Nav.Link href="#action2">Link</Nav.Link>
+                <NavDropdown
+                  title="Dropdown"
+                  id="offcanvasNavbarDropdown-expand-sm"
+                >
+                  <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action4">
+                    Another action
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action5">
+                    Something else here
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+              <Form className="d-flex">
+                <Form.Control
+                  type="search"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
+                />
+                <Button variant="outline-success">Search</Button>
+              </Form>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Container>
+      </Navbar>
+    </>
   );
-};
-
+}
 export default CustomNavbar;
